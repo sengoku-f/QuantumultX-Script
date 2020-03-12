6e7dc278c954f2d906fb4f4c26b8af9d664e7623
sengoku-f <github@donxj.com>
2020-03-13 00:36:01 +0800

    删除

--- saigaocy/saigaocy.cookie.js
+++ saigaocy/saigaocy.cookie.js
@@ -6,30 +6,7 @@
 saigaocy.com
 
 */
-/* 
-let headerCookie = $request.headers["Cookie"];
 
-if (headerCookie) {
-  if ($prefs.valueForKey("saigaocyCookie") != undefined) {
-    if ($prefs.valueForKey("saigaocyCookie") != headerCookie) {
-      var cookie = $prefs.setValueForKey(headerCookie, "saigaocyCookie");
-      if (!cookie) {
-        $notify("更新赛高次元Cookie失败！", "", "");
-      } else {
-        $notify("更新赛高次元Cookie成功！", "", "");
-      }
-    }
-  } else {
-    let cookie = $prefs.setValueForKey(headerCookie, "saigaocyCookie");
-    if (!cookie) {
-      $notify("首次写入赛高次元Cookie失败！", "", "");
-    } else {
-      $notify("首次写入赛高次元Cookie成功！", "", "");
-    }
-  }
-}
-$done({});
- */
 const cookieName = 'saigaocy'
 const cookieKey = 'chavy_cookie_saigaocy'
 const chavy = init()
