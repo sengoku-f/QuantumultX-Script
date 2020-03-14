1d35c2701fed3bc299b1bebec3d861f730077784
sengoku-f <github@donxj.com>
2020-03-14 23:39:44 +0800

    添加芯次元

---
+++ xinciyuan/xinciyuan.js
@@ -0,0 +1,83 @@
+const cookieName = '芯次元'
+const cookieKey = 'chavy_cookie_xinciyuan'
+const chavy = init()
+const cookieVal = chavy.getdata(cookieKey)
+
+sign()
+
+function sign() {
+  let url = {
+    url: `https://acg.ge/wp-admin/admin-ajax.php`,
+    headers: {
+      Cookie: cookieVal
+    }
+  }
+  url.headers['Referer'] = 'https://acg.ge/'
+  url.headers['Path'] = '/wp-admin/admin-ajax.php?_nonce=5940db0f04&action=0f00b9c5cfbbecc13be33c9d146f2d42&type=goSign'
+  url.headers['Accept'] = '*/*'
+  url.headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.4 Safari/605.1.15'
+
+  chavy.get(url, (error, response, data) => {
+    let result = JSON.parse(data)
+    let title = `${cookieName}`
+    // 签到成功
+    if (result && result.code == 0) {
+      let subTitle = `签到结果: 成功🎉`
+      let detail = `说明: ${result.data.msg}`
+      chavy.msg(title, subTitle, detail)
+    }
+    // 签到失败
+    else {
+      let subTitle = `签到结果: 失败`
+      let detail = `说明: ${result.message}`
+      chavy.msg(title, subTitle, detail)
+    }
+    chavy.log(`${cookieName}, data: ${data}`)
+  })
+
+  chavy.done()
+}
+
+function init() {
+  isSurge = () => {
+    return undefined === this.$httpClient ? false : true
+  }
+  isQuanX = () => {
+    return undefined === this.$task ? false : true
+  }
+  getdata = (key) => {
+    if (isSurge()) return $persistentStore.read(key)
+    if (isQuanX()) return $prefs.valueForKey(key)
+  }
+  setdata = (key, val) => {
+    if (isSurge()) return $persistentStore.write(key, val)
+    if (isQuanX()) return $prefs.setValueForKey(key, val)
+  }
+  msg = (title, subtitle, body) => {
+    if (isSurge()) $notification.post(title, subtitle, body)
+    if (isQuanX()) $notify(title, subtitle, body)
+  }
+  log = (message) => console.log(message)
+  get = (url, cb) => {
+    if (isSurge()) {
+      $httpClient.get(url, cb)
+    }
+    if (isQuanX()) {
+      url.method = 'GET'
+      $task.fetch(url).then((resp) => cb(null, {}, resp.body))
+    }
+  }
+  post = (url, cb) => {
+    if (isSurge()) {
+      $httpClient.post(url, cb)
+    }
+    if (isQuanX()) {
+      url.method = 'POST'
+      $task.fetch(url).then((resp) => cb(null, {}, resp.body))
+    }
+  }
+  done = (value = {}) => {
+    $done(value)
+  }
+  return { isSurge, isQuanX, msg, log, getdata, setdata, get, post, done }
+}
