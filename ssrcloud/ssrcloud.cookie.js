a86f972f0e9a8b104cfc35b51500c3b1d1946157
sengoku-f <github@donxj.com>
2020-07-18 13:19:28 +0800

    添加[🍟薯条签到]

---
+++ ssrcloud/ssrcloud.cookie.js
@@ -0,0 +1,63 @@
+/*
+
+[rewrite_local]
+^https:\/\/ntt-co-jp\.club\/user url script-request-header sengoku/ssrcloud/ssrcloud.cookie.js
+[mitm]
+flycloud.win
+
+*/
+
+const cookieName = '🍟薯条'
+const cookieKey = 'chavy_cookie_ssrcloud'
+const chavy = init()
+const cookieVal = $request.headers['Cookie']
+if (cookieVal) {
+  if (chavy.setdata(cookieVal, cookieKey)) {
+    chavy.msg(`${cookieName}`, '获取Cookie: 成功', '')
+    chavy.log(`[${cookieName}] 获取Cookie: 成功, cookie: ${cookieVal}`)
+  }
+}
+function init() {
+  isSurge = () => {
+    return undefined === this.$httpClient ? false : true
+  }
+  isQuanX = () => {
+    return undefined === this.$task ? false : true
+  }
+  getdata = (key) => {
+    if (isSurge()) return $persistentStore.read(key)
+    if (isQuanX()) return $prefs.valueForKey(key)
+  }
+  setdata = (key, val) => {
+    if (isSurge()) return $persistentStore.write(key, val)
+    if (isQuanX()) return $prefs.setValueForKey(key, val)
+  }
+  msg = (title, subtitle, body) => {
+    if (isSurge()) $notification.post(title, subtitle, body)
+    if (isQuanX()) $notify(title, subtitle, body)
+  }
+  log = (message) => console.log(message)
+  get = (url, cb) => {
+    if (isSurge()) {
+      $httpClient.get(url, cb)
+    }
+    if (isQuanX()) {
+      url.method = 'GET'
+      $task.fetch(url).then((resp) => cb(null, {}, resp.body))
+    }
+  }
+  post = (url, cb) => {
+    if (isSurge()) {
+      $httpClient.post(url, cb)
+    }
+    if (isQuanX()) {
+      url.method = 'POST'
+      $task.fetch(url).then((resp) => cb(null, {}, resp.body))
+    }
+  }
+  done = (value = {}) => {
+    $done(value)
+  }
+  return { isSurge, isQuanX, msg, log, getdata, setdata, get, post, done }
+}
+chavy.done()
